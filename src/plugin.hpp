77fded22e8e9a5a01284445af06c4e0300232d64
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-11-13 22:14:08 -0600

    Add: collected plugin.hpp

---
+++ src/plugin.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <rack.hpp>
+#ifndef USING_CARDINAL_NOT_RACK
+#include "themes.hpp"
+#else
+#include "../SanguineModulesCommon/src/themes.hpp"
+#endif
+
+using namespace rack;
+
+extern Plugin* pluginInstance;
+
+// Mutants
+extern Model* modelFunes;
+extern Model* modelMutantsBlank;
+extern Model* modelApices;
+extern Model* modelAleae;
+extern Model* modelNodi;
+extern Model* modelContextus;
+extern Model* modelNebulae;
+extern Model* modelEtesia;
+extern Model* modelMortuus;
+extern Model* modelFluctus;
+extern Model* modelIncurvationes;
+extern Model* modelDistortiones;
+extern Model* modelMutuus;
+extern Model* modelExplorator;
+extern Model* modelMarmora;
+extern Model* modelAnuli;
+extern Model* modelVelamina;
+extern Model* modelAestus;
+extern Model* modelTemulenti;
+extern Model* modelVimina;
+extern Model* modelScalaria;
+extern Model* modelReticula;
+extern Model* modelFunesMk2;
+extern Model* modelNix;
+extern Model* modelAnsa;
+
+
+// Monsters
+extern Model* modelSuperSwitch81;
+extern Model* modelSuperSwitch18;
+extern Model* modelMonstersBlank;
+extern Model* modelDollyX;
+extern Model* modelOraculus;
+extern Model* modelRaiju;
+extern Model* modelSphinx;
+extern Model* modelBrainz;
+extern Model* modelBukavac;
+extern Model* modelDungeon;
+extern Model* modelKitsune;
+extern Model* modelOubliette;
+extern Model* modelMedusa;
+extern Model* modelAion;
+extern Model* modelWerewolf;
+extern Model* modelAlchemist;
+extern Model* modelChronos;
+extern Model* modelFortuna;
+extern Model* modelAionKairos;
+extern Model* modelAlembic;
+extern Model* modelDenki;
+extern Model* modelManus;
+extern Model* modelCrucible;
